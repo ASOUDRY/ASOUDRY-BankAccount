07b4c2dd614593b75117ba394200ad3b08fd5d12
ASOUDRY <soudryalex@gmail.com>
2023-02-19 18:25:35 +0000

    done

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -24,7 +24,7 @@ public class BankService {
      */
     public void deposit(double amount){
         this.balance += amount;
-        // placeholder
+        // placeholderhhhhhhh
     }
 
     /**
